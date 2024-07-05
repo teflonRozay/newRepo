8ea3605c045775e38b7902dc26f6a56114f0401e
Timmy-Edibo <timothyedibo@gmail.com>
2024-07-05 21:57:17 +0100

    update reset password ui

--- src/Components/ResetPassword.js
+++ src/Components/ResetPassword.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button.js";
+
 
 const ResetPassword = () => {
   const [codeDigits, setCodeDigits] = useState(["", "", "", "", "", ""]);
   const [verificationError, setVerificationError] = useState("");
   const navigate = useNavigate();
-  const baseUrl =process.env.REACT_APP_BASEURL
+  const baseUrl = process.env.REACT_APP_BASEURL;
 
+  
   const handleChange = (e, index) => {
     const newCodeDigits = [...codeDigits];
     newCodeDigits[index] = e.target.value;
@@ -14,7 +17,7 @@ const ResetPassword = () => {
   };
 
   const buttonStyle = {
-    margin: '2rem',
+    margin: "2rem",
     // Add more styles as needed
   };
 
@@ -29,14 +32,14 @@ const ResetPassword = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          token:verificationCode,
-          triggerEvent:'confirm-reset-password'
+          token: verificationCode,
+          triggerEvent: "confirm-reset-password",
         }),
       });
 
       if (response.ok) {
-        const data = await response.json()
-        console.log("validation token: " + data)
+        const data = await response.json();
+        console.log("validation token: " + data);
         navigate(`/PasswordUpdate/${data.data.token}`);
       } else {
         setVerificationError("Incorrect verification code. Please try again.");
@@ -49,26 +52,41 @@ const ResetPassword = () => {
 
   return (
     <div>
-      <h2>Verify Email</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Enter Verification Code:</label>
+      <div className="flex flex-col justify-center items-center text-center border mx-[20rem] h-screen bg-white">
         <div>
-          {codeDigits.map((digit, index) => (
-            <input
-              key={index}
-              type="number"
-              min="0"
-              max="9"
-              value={digit}
-              onChange={(e) => handleChange(e, index)}
-              required
-              style={{ width: "2em", textAlign: "center" }}
-            />
-          ))}
+          {/* <h2>Verify Email</h2> */}
         </div>
-        <button type="submit" style={buttonStyle}>Submit</button>
-      </form>
-      {verificationError && <p style={{ color: "red" }}>{verificationError}</p>}
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label className="font-bold text-2xl">Enter Verification Code</label>
+            <div>
+              {codeDigits.map((digit, index) => (
+                <input
+                  key={index}
+                  type="number"
+                  min="0"
+                  max="9"
+                  value={digit}
+                  onChange={(e) => handleChange(e, index)}
+                  required
+                  className=" w-[3em] h-[3em] text-center input-no-spinner  border rounded-lg border-black mx-2 my-4"
+                />
+              ))}
+            </div>
+            {/* <button type="submit" style={buttonStyle}>
+              Submit
+            </button> */}
+            <Button
+              type="submit"
+              className="bg-purple-900 px-2"
+              text="Submit"
+            ></Button>
+          </form>
+          {verificationError && (
+            <p style={{ color: "red" }}>{verificationError}</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
